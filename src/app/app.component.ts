c38162a02241dc14075850cf5d8723abed8d8db7
Austin <amcdaniel2@gmail.com>
2017-12-17 12:12:26 -0600

    dashboard schematic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  cards = [
+    {
+      title: 'Sales Leaders'
+    },
+    {
+      title: 'Sales Goal'
+    },
+    {
+      title: 'Top Sales'
+    },
+    {
+      title: 'Pending Sales'
+    }
+  ];
+
+  ngOnInit() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(() => {
+      const data = google.visualization.arrayToDataTable([
+        ['Customer', 'Sale Price'],
+        ['Corp A', 10000],
+        ['Corp B', 2000],
+        ['Corp C', 5000],
+        ['Corp D', 1000]
+      ]);
+
+      for (const elm of (<any>document.getElementsByClassName('chart'))) {
+        const chart = new google.visualization.PieChart(elm);
+        chart.draw(data, {});
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatGridListModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 
@@ -11,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
