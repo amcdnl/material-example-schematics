f137977e631b537cafda74e6941be99eb95fb16e
Austin <amcdaniel2@gmail.com>
2018-02-18 11:15:26 -0600

    tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 declare var google: any;
 
@@ -7,37 +7,19 @@ declare var google: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   cards = [
     {
-      title: 'Sales Leaders'
+      title: 'Card 1'
     },
     {
-      title: 'Sales Goal'
+      title: 'Card 2'
     },
     {
-      title: 'Top Sales'
+      title: 'Card 3'
     },
     {
-      title: 'Pending Sales'
+      title: 'Card 4'
     }
   ];
-
-  ngOnInit() {
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(() => {
-      const data = google.visualization.arrayToDataTable([
-        ['Customer', 'Sale Price'],
-        ['Corp A', 10000],
-        ['Corp B', 2000],
-        ['Corp C', 5000],
-        ['Corp D', 1000]
-      ]);
-
-      for (const elm of (<any>document.getElementsByClassName('chart'))) {
-        const chart = new google.visualization.PieChart(elm);
-        chart.draw(data, {});
-      }
-    });
-  }
 }
